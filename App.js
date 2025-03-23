c6d071962f0d8fa0b31e8edfdfac5bc4cdcfcb01
Girija Shankar ray <91793906+Girijaray07@users.noreply.github.com>
2025-03-23 19:49:24 +0530

    Modified the Logic

--- App.js
+++ App.js
@@ -9,16 +9,26 @@ const app = express();
 const port = 3000;
 const OPENWEATHER_APIKEY = process.env.OPENWEATHER_APIKEY_MAIN;
 
+var weather_data, location, state, country, formattedDate, sunRise, sunSet;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        data: weather_data,
+        location: location,
+        state: state,
+        country: country,
+        date: formattedDate,
+        sunrise: sunRise,
+        sunset: sunSet
+    });
 });
 
 app.post("/submit", async (req, res) => {
-    var location = req.body.location;
+    location = req.body.location;
 
     if (!(location)) {
         res.redirect("/");
@@ -31,20 +41,19 @@ app.post("/submit", async (req, res) => {
         const lat = data[0].lat;
         const lon = data[0].lon;
         location = data[0].name;
-        const state = data[0].state;
-        const country = data[0].country;
+        state = data[0].state;
+        country = data[0].country;
 
-        console.log(lat, lon, location, state, country);
+        console.log(lat, lon, location, state, country + " - " + new Date().toLocaleTimeString());
 
         const weather_response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_APIKEY}&units=metric`);
 
-        const weather_data = weather_response.data;
-        // console.log(weather_data);
+        weather_data = weather_response.data;
 
         const timestamp = weather_data.dt;
         const timezoneOffset = weather_data.timezone;
         const localTime = new Date((timestamp + timezoneOffset) * 1000);
-        const formattedDate = localTime.toLocaleDateString("en-US", {
+        formattedDate = localTime.toLocaleDateString("en-US", {
             weekday: "long",
             day: "numeric",
             month: "long"
@@ -53,41 +62,28 @@ app.post("/submit", async (req, res) => {
         const sunriseLocal = new Date(weather_data.sys.sunrise * 1000);
         const sunsetLocal = new Date(weather_data.sys.sunset * 1000);
 
-        const sunRise = sunriseLocal.toLocaleTimeString("en-US", {
+        sunRise = sunriseLocal.toLocaleTimeString("en-US", {
             timeZone: "Asia/Kolkata",
             hour: "2-digit",
             minute: "2-digit",
             hour12: true
         });
 
-        const sunSet = sunsetLocal.toLocaleTimeString("en-US", {
+        sunSet = sunsetLocal.toLocaleTimeString("en-US", {
             timeZone: "Asia/Kolkata",
             hour: "2-digit",
             minute: "2-digit",
             hour12: true
         });
 
-        console.log(sunRise, sunSet);
 
-        res.render("index.ejs", {
-            data: weather_data,
-            location: location,
-            state: state,
-            country: country,
-            date: formattedDate,
-            sunrise: sunRise,
-            sunset: sunSet
-        });
+        res.redirect("/");
     } catch (err) {
         console.log(err);
     }
 });
 
-app.get("/submit", (req, res) => {
-    res.render("index.ejs");
-})
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     console.log(`http://localhost:${port}/`);
-})
+});
